bc73bfa9ee2518754ac97432c4a269a81ac5fa75
Vlad Nitu <vlnitu@eu-login-45.euler.ethz.ch>
2024-11-19 20:49:36 +0100

    feat(hpc): rerun correctly now Pythia experimetns

--- generate_lab4_tlist.py
+++ generate_lab4_tlist.py
@@ -5,16 +5,16 @@ trace_directory = "traces/"
 output_file = "experiments/lab4-prefetching.tlist"
 
 # KNOBS constant value
-knobs_value = "--knob_cloudsuite=true --warmup_instructions=100000000 --simulation_instructions=150000000"
 
 # Get all filenames in the specified directory
 trace_files = os.listdir(trace_directory)
 
 # Open the output file and write entries for each trace file
 with open(output_file, "w") as file:
-        for trace in trace_files:
-            file.write(f"NAME={trace}\n")
-            file.write(f"TRACE=$(PYTHIA_HOME)/traces/{trace}\n")
-            file.write(f"KNOBS={knobs_value}\n\n")
-
-print(f"Created {output_file} with entries for traces in {trace_directory}.")
+            for trace in trace_files:
+                            # Extract the desired part of the trace name
+                            trace_name = '.'.join(trace.split('.')[:2])
+                            file.write(f"NAME={trace_name}\n")
+                            file.write(f"TRACE=$(PYTHIA_HOME)/traces/{trace}\n")
+                            file.write(f"KNOBS=\n")
+            print(f"Created {output_file} with entries for traces in {trace_directory}.")
